652417ffa5c62b2080520d203d091466acbbed4b
kazz54 <jonathan.mrutu@gmail.com>
2023-11-17 12:21:59 +0300

    Update gatsby-browser.js

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,4 +1,5 @@
-import './src/css/main.css'
-import './src/css/bootstrap-grid.min.css'
 //require("prismjs/themes/prism-solarizedlight.css")
 require("prismjs/themes/prism-dark.css")
+import './src/css/main.css'
+import './src/css/bootstrap-grid.min.css'
+
