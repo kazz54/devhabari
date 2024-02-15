0ac8be1477cbe9330ebe4212b9b9b023bf32c00b
kazz54 <jonathan.mrutu@gmail.com>
2024-02-15 09:00:15 +0300

    Update post-template.jsx

--- src/templates/post-template.jsx
+++ src/templates/post-template.jsx
@@ -15,7 +15,7 @@ import Seo from '../components/SEO'
 // import slugify from 'slugify'
 
 // import { Helmet } from 'react-helmet'
-
+const { words } = fields.timeToRead
 const PostTemplate = ({ data, children }) => {
   const {
     mdx: {
