ef1f9fd790d20f2f6dea71a0dd61a12c299e91a2
kazz54 <jonathan.mrutu@gmail.com>
2024-02-18 15:42:33 +0300

    Update Network.jsx

--- src/components/About/Network.jsx
+++ src/components/About/Network.jsx
@@ -15,9 +15,17 @@ const Network = () => {
 
             <div className="openmastery">
               <AiOutlineDeploymentUnit className="icon" />
-              <h3>Partner 1</h3>
+             <h3>Mkakati</h3>
               <p>
-              Adaptogen skateboard schlitz viral edison bulb knausgaard shabby chic drinking vinegar semiotics cred occupy. Fit roof party ugh fanny pack ascot. Blog messenger bag you probably haven't heard of them distillery, vexillologist offal brunch leggings gentrify sustainable mumblecore pok pok polaroid pour-over big mood. 
+               DevHabiri ni tuvuti ambayo lengo lake niku chapisha makala za jinsi ambayo unaweza kutumia tekinologia katika mazingira yako ili kurahisisha shuhuli mbalimbali, hapa utapata mbinu na dondoo mbali mbali za jinsi ambanyo unaweza kutumia tekinologia.
+
+Tuliamua kuchagua lugha ya kiswahili kutokana na uhaba wa makala kamahizi kwa lugha hii "Kiswahili".
+
+Sababu nyingine kubwa zaidi ni kwamba lugha za tekinologia ya komputa zimeandikwa kwajili ya binadamua kusoma na kuelewa alafu mashine "komputa" huzitumia "execute" pale zinapo hitaji.
+
+kwa mantiki hiyo basi sisi tumeamua kuzisimulia(chambua)/"fafanua" kwa lugha ya kiswahili
+
+ili iwe rahisi kwa mtu yoyote yule amabaye anajua lughaa ya kiswahili kufuatilia makala hizi na hatimaye kunufaika nazo kwa namna moja au nyingine.
               </p>
               <p>
                 <a href="#" target="_blank" rel="noopener noreferrer">Check it out</a>
@@ -25,9 +33,9 @@ const Network = () => {
             </div>
             <div className="openjournal">
               <AiOutlineDeploymentUnit className="icon" />
-              <h3>Partner 2</h3>
+                <h3>Maono</h3>
               <p>
-              Adaptogen skateboard schlitz viral edison bulb knausgaard shabby chic drinking vinegar semiotics cred occupy.
+              Jamii ya watu wanao endesha shuhuli zao kwa kutumia tekinologia, kiswahili na tekinologia inawezekana.  
               </p>
             </div>
             <div className="network-card">
