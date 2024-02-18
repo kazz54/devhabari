ff46c0607aaa8b5be168d953404e9011c6d846f4
kazz54 <jonathan.mrutu@gmail.com>
2024-02-18 11:15:04 +0300

    Update contact.jsx

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -2,17 +2,18 @@ import React from 'react'
 import Layout from '../components/Layout'
 import Seo from '../components/SEO'
 import ContactForm from '../components/ContactForm'
-import AboutUs from '../components/About/AboutUs'
+//import AboutUs from '../components/About/AboutUs'
+import Network from '../components/About/Network'
 
 
 const contact = () => {
   return (
     <Layout>
       <Seo
-        title="Contact Us | Revista"
+        title="Contact Us | Devhabari"
         description="Reach out for any inquiries, or find out how you can help our mission!"
       />
-       <AboutUs />
+       <Network />
       <ContactForm />
     </Layout>
   )
